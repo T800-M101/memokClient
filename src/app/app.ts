ff9bf451b1bfab411907812ee2f03a6d4244d29f
Guillermo Morán <tuahil@gmail.com>
2026-06-09 19:59:35 -0600

    add host listener to close overlay-menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { Topbar } from './features/topbar/topbar';
 import { Sidebar } from './features/sidebar/sidebar';
 import { WorkingArea } from './features/working-area/working-area';
 import { OverlayMenu } from './features/overlay-menu/overlay-menu';
+import { OverlayMenuService } from './core/overlay-menu.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,18 @@ import { OverlayMenu } from './features/overlay-menu/overlay-menu';
 })
 export class App {
   protected readonly title = signal('memok-client');
+  overlayMenuService = inject(OverlayMenuService);
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const isMenuButton = target.closest('.menu-toggle');
+    const isOverlayMenu = target.closest('.overlay-menu');
+
+    if (this.overlayMenuService.isMenuOpen() && !isMenuButton && !isOverlayMenu) {
+      this.overlayMenuService.closeMenu();
+    }
+  }
 
 
 }
