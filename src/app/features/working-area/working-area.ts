ca0c35394421dbb15b09290d08ac096b6ae2f22d
Guillermo Morán <tuahil@gmail.com>
2026-06-13 12:06:18 -0600

    improve message in working area

--- src/app/features/working-area/working-area.ts
+++ src/app/features/working-area/working-area.ts
@@ -1,18 +1,34 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RequestBar } from './request-bar/request-bar';
 import { ConfigBar } from './config-bar/config-bar';
 import { ResponseSection } from './response-section/response-section';
 import { RequestsService } from '../../core/services/requests-service/requests-service';
+import { ApiRequest } from '../../core/interfaces/api-request.interface';
 
 @Component({
   selector: 'app-working-area',
-  imports: [RequestBar, ConfigBar, ResponseSection],
+  standalone: true,
+  imports: [CommonModule, RequestBar, ConfigBar, ResponseSection],
   templateUrl: './working-area.html',
-  styleUrl: './working-area.scss',
+  styleUrls: ['./working-area.scss'],
 })
 export class WorkingArea {
   private requestsService = inject(RequestsService);
 
   activeRequest = this.requestsService.activeRequest;
 
+  createNewRequest(): void {
+    const emptyRequest: ApiRequest = {
+      requestId: crypto.randomUUID(),
+      name: 'Untitled Request',
+      method: 'GET',
+      url: '',
+      params: {},
+      headers: {},
+      auth: { type: 'none' },
+      body: null,
+    };
+    this.requestsService.setActiveRequest('', emptyRequest);
+  }
 }
